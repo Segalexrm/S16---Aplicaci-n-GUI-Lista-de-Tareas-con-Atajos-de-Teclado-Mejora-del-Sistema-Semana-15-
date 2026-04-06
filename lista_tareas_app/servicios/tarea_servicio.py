29c8c2c9fddbae46e75214adb340b84adac37b4f
Segalexrm <alexrosado1996@hotmail.com>
2026-04-05 19:42:22 -0500

    se actualiza tarea_servicio,py

--- lista_tareas_app/servicios/tarea_servicio.py
+++ lista_tareas_app/servicios/tarea_servicio.py
@@ -1,27 +1,21 @@
 from modelos.tarea import Tarea
-from typing import List
 
 class TareaServicio:
     def __init__(self):
-        # Simulamos una base de datos en memoria
-        self._tareas: List[Tarea] = []
+        self._tareas = []
 
-    def agregar_tarea(self, descripcion: str):
-        nueva_tarea = Tarea(descripcion)
-        self._tareas.append(nueva_tarea)
-        return nueva_tarea
-
-    def obtener_todas(self) -> List[Tarea]:
-        return self._tareas
-
-    def eliminar_tarea(self, indice: int):
-        if 0 <= indice < len(self._tareas):
-            self._tareas.pop(indice)
-        else:
-            raise IndexError("La tarea seleccionada no existe.")
+    def agregar_tarea(self, texto: str):
+        nueva = Tarea(texto)
+        self._tareas.append(nueva)
+        return nueva
 
     def marcar_completada(self, indice: int):
         if 0 <= indice < len(self._tareas):
             self._tareas[indice].completada = True
-        else:
-            raise IndexError("La tarea seleccionada no existe.")
+
+    def eliminar_tarea(self, indice: int):
+        if 0 <= indice < len(self._tareas):
+            self._tareas.pop(indice)
+
+    def obtener_todas(self):
+        return self._tareas
